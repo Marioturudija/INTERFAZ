aa04445fb541490b62326ad2276deb03ac891ab3
Mario <mario@gmail.com>
2022-05-25 09:57:52 +0200

    Pimer commit

---
+++ src/application/SampleController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class SampleController {
+	@FXML Button INICIAR;
+	
+	void metodo(MouseEvent event) {
+		
+	}
+	
+}
